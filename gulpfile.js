ddde7b9d1a4818fee988d09a25d8f83886c5c682
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2014-04-11 17:44:38 +0200

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -31,11 +31,12 @@
     "public": 'www',
     styles: 'www/css',
     scripts: 'www/js',
-    templates: 'www'
+    templates: 'www',
+    livereload: ['www/**']
   };
 
   var options = {
-    open: false,
+    open: true,
     httpPort: 4400,
     riddlePort: 4400
   };
@@ -66,7 +67,7 @@
       .pipe(concat('vendor.js'))
       .pipe(gulp.dest(destinations.scripts));
 
-    return gulp.src(paths.scripts.app))
+    return gulp.src(paths.scripts.app)
       .pipe(coffee({
         sourceMap: false
       }))
@@ -87,7 +88,7 @@
   });
 
   gulp.task('watch', function() {
-    var livereloadServer;
+    var livereloadServer = livereload();
 
     gulp.watch(paths["public"], ['copy_public']);
     gulp.watch(paths.scripts.app, ['scripts']);
@@ -95,43 +96,53 @@
     gulp.watch(paths.styles, ['styles']);
     gulp.watch(paths.templates, ['templates']);
 
-    livereloadServer = livereload();
-    
-    return gulp.watch('www/**/*.css').on('change', function(file) {
+    return gulp.watch(destinations.livereload).on('change', function(file) {
       return livereloadServer.changed(file.path);
     });
   });
 
   gulp.task('emulator', function() {
-    var url;
+    var url = "http://localhost:" + options.ripplePort + "/?enableripple=cordova-3.0.0-HVGA";
+
     ripple.emulate.start(options);
+
     gutil.log(gutil.colors.blue("Ripple-Emulator listening on " + options.ripplePort));
+
     if (options.open) {
-      url = "http://localhost:" + options.ripplePort + "/?enableripple=cordova-3.0.0-HVGA";
       open(url);
       return gutil.log(gutil.colors.blue("Opening " + url + " in the browser..."));
     }
   });
 
   gulp.task('server', function() {
-    var url;
+    var url = "http://localhost:" + options.httpPort + "/";
+
     http.createServer(ecstatic({
       root: "www"
     })).listen(options.httpPort);
+
     gutil.log(gutil.colors.blue("HTTP server listening on " + options.httpPort));
+
     if (options.open) {
-      url = "http://localhost:" + options.httpPort + "/";
       open(url);
       return gutil.log(gutil.colors.blue("Opening " + url + " in the browser..."));
     }
   });
 
   gulp.task('build', function(cb) {
-    return runSequence('clean', ['copy_public', 'styles', 'scripts', 'templates'], cb);
+    return runSequence(
+      'clean', 
+      ['copy_public', 'styles', 'scripts', 'templates'], 
+      cb
+    );
   });
 
   gulp.task('default', function(cb) {
-    return runSequence('build', ['watch', 'server'], cb);
+    return runSequence(
+      'build', 
+      ['watch', 'server'],
+      cb
+    );
   });
 
 }).call(this);
